0355c69d0b8a02b8347c33e4ee469b5a165b3cb6
Dylan Connor <dconnor@req.co>
2019-04-23 11:24:03 -0400

    set up webpack, react, etc

--- src/js/components/container/Container.jsx
+++ src/js/components/container/Container.jsx
@@ -1,2 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+
+class Container extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <h1>hello</h1>
+        );
+    }
+}
+export default Container;
+
+const wrapper = document.getElementById("app");
+wrapper ? ReactDOM.render(<Container />, wrapper) : false;
